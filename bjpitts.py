6abf45df11dbbd977f17b8526d03d0f3c708fe5f
Brandon Pitts <bjamespitts@yahoo.com>
2014-07-30 15:50:18 -0700

    bjpitts: initial commit, first python program

---
+++ bjpitts.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+def funtwo(superman):
+    print "in function %s" % superman
+
+def funone():
+    print "in function one"
+
+def main():
+    funone();
+    funtwo("two");
+
+main()
+
